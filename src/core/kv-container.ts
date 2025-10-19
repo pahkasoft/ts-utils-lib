e97d01351e63d656a335c1ec399d58b48a2562cb
S <77819265+pahkasoft@users.noreply.github.com>
2025-10-20 00:19:30 +0300

    refactor: renamed file.

---
+++ src/core/kv-container.ts
@@ -0,0 +1,12 @@
+
+export interface KVComponent<K extends any[], EL> {
+    get size(): number;
+    has(...keys: K): boolean;
+    get(...keys: K): EL | undefined;
+    getOrDefault(...keysAndDefault: [...K, EL]): EL;
+    getOrCreate(...keysAndCreator: [...K, EL]): EL;
+    set(...keysAndValue: [...K, EL]): void;
+    delete(...keys: K): boolean;
+    clear?(): void;
+    toString(): string;
+}
