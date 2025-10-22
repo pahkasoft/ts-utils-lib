c9a1003f03d8263657073a210e9b9b4fa0bc4b76
S <77819265+pahkasoft@users.noreply.github.com>
2025-10-22 14:00:46 +0300

    feat: edit map utilities and add tests.

--- src/utils/map/index.ts
+++ src/utils/map/index.ts
@@ -1,6 +1,4 @@
 
 export function getMapKeys<K, V>(map: Map<K, V>): K[] {
-    let keys: K[] = [];
-    map.forEach((value: V, key: K) => keys.push(key));
-    return keys;
+    return [...map.keys()];
 }
