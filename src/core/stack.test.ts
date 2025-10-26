fc50efa6d4edf771db91b425f0f185657ed5cb74
S <77819265+pahkasoft@users.noreply.github.com>
2025-10-26 12:51:24 +0200

    feat: improved Stack.

--- src/core/stack.test.ts
+++ src/core/stack.test.ts
@@ -1,15 +1,99 @@
 import { Stack } from "./stack";
 
 describe(Stack.name, () => {
-    it("should work", () => {
-        let s = new Stack<number>();
-        expect(s.push(7)).toEqual(7);
-        expect(s.push(-1)).toEqual(-1);
-        expect(s.push(NaN)).toEqual(NaN);
-        expect(s.toArray()).toEqual([7, -1, NaN]);
-        expect(s.pop()).toEqual(NaN);
-        expect(s.pop()).toEqual(-1);
-        expect(s.pop()).toEqual(7);
+
+    let s: Stack<number>;
+
+    beforeEach(() => {
+        s = new Stack<number>();
+        s.push(0);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+    });
+
+    it("top() returns top or throws", () => {
+        expect(s.top()).toBe(3);
+        s.clear();
+        expect(() => s.top()).toThrow();
+    });
+
+    it("topOrUndef() returns top or undefined", () => {
+        expect(s.topOrUndef()).toBe(3);
+        s.clear();
+        expect(s.topOrUndef()).toBeUndefined();
+    });
+
+    it("topOr() returns top or default", () => {
+        expect(s.topOr(-1)).toBe(3);
+        s.clear();
+        expect(s.topOr(-1)).toBe(-1);
+    });
+
+    it("pop() pops or throws", () => {
+        expect(s.pop()).toBe(3);
+        expect(s.pop()).toBe(2);
+        expect(s.pop()).toBe(1);
+        expect(s.pop()).toBe(0);
         expect(() => s.pop()).toThrow();
+    });
+
+    it("popOrUndef() pops or returns undefined", () => {
+        expect(s.popOrUndef()).toBe(3);
+        expect(s.popOrUndef()).toBe(2);
+        expect(s.popOrUndef()).toBe(1);
+        expect(s.popOrUndef()).toBe(0);
+        expect(s.popOrUndef()).toBeUndefined();
+    });
+    
+    it("popOr() pops or returns default", () => {
+        expect(s.popOr(-1)).toBe(3);
+        expect(s.popOr(-1)).toBe(2);
+        expect(s.popOr(() => -1)).toBe(1);
+        expect(s.popOr(() => -1)).toBe(0);
+        expect(s.popOr(-1)).toBe(-1);
+        expect(s.popOr(() => -1)).toBe(-1);
+    });
+
+    it("peek() peeks or throws", () => {
+        expect(() => s.peek(-1)).toThrow();
+        expect(s.peek()).toBe(3);
+        expect(s.peek(0)).toBe(3);
+        expect(s.peek(1)).toBe(2);
+        expect(s.peek(2)).toBe(1);
+        expect(s.peek(3)).toBe(0);
+        expect(() => s.peek(4)).toThrow();
+    });
+
+    it("peek() peeks or throws", () => {
+        expect(() => s.peek(-1)).toThrow();
+        expect(s.peek()).toBe(3);
+        expect(s.peek(0)).toBe(3);
+        expect(s.peek(1)).toBe(2);
+        expect(s.peek(2)).toBe(1);
+        expect(s.peek(3)).toBe(0);
+        expect(() => s.peek(4)).toThrow();
+    });
+
+    it("peekOrUndef() peeks or return undefined", () => {
+        expect(s.peekOrUndef(-1)).toBeUndefined();
+        expect(s.peekOrUndef()).toBe(3);
+        expect(s.peekOrUndef(0)).toBe(3);
+        expect(s.peekOrUndef(1)).toBe(2);
+        expect(s.peekOrUndef(2)).toBe(1);
+        expect(s.peekOrUndef(3)).toBe(0);
+        expect(s.peekOrUndef(4)).toBeUndefined();
+    });
+
+    it("clear() and isEmpty()", () => {
+        expect(s.isEmpty()).toBeFalse();
+        s.clear();
+        expect(s.isEmpty()).toBeTrue();
+    });
+
+    it("toArray()", () => {
+        expect(s.toArray()).toEqual([0, 1, 2, 3]);
+        s.clear();
+        expect(s.toArray()).toEqual([]);
     });
 });
--- src/core/stack.ts
+++ src/core/stack.ts
@@ -1,31 +1,87 @@
+import { isFunction } from "../guard";
 import { Assert } from "..";
 
 export class Stack<T> {
-    private readonly list: T[] = [];
+    private readonly data: T[] = [];
 
-    push(e: T) {
-        this.list.push(e);
-        return e;
-    }
-
-    pop(): T {
-        Assert.int_gt(this.list.length, 0);
-        return this.list.pop()!;
-    }
-
-    top() {
-        return Assert.array_elem(this.list, this.list.length - 1, "Stack is empty!");
-    }
-
-    toArray() {
-        return this.list;
+    private assertId(id: number): number {
+        Assert.isIntegerBetween(id, 0, this.length - 1, `Stack index ${id} out of range.`);
+        return id;
     }
 
     get length() {
-        return this.list.length;
+        return this.data.length;
+    }
+
+    push(el: T) {
+        this.data.push(el);
+        return el;
+    }
+
+    pop(): T {
+        this.assertId(this.length - 1);
+        return this.data.pop()!;
+    }
+
+    popOrUndef(): T | undefined {
+        return this.data.pop();
+    }
+
+    popOr(defaultValueOrCreator: T | (() => T)): T {
+        return this.isEmpty()
+            ? (isFunction(defaultValueOrCreator) ? defaultValueOrCreator() : defaultValueOrCreator)
+            : this.data.pop()!;
+    }
+
+    top() {
+        return this.data[this.assertId(this.length - 1)];
+    }
+
+    topOrUndef(): T | undefined {
+        return this.data[this.length - 1];
+    }
+
+    topOr(defaultValueOrCreator: T | (() => T)): T {
+        return this.isEmpty()
+            ? (isFunction(defaultValueOrCreator) ? defaultValueOrCreator() : defaultValueOrCreator)
+            : this.data[this.length - 1];
+    }
+
+    peek(depth = 0): T {
+        return this.data[this.assertId(this.data.length - 1 - depth)];
+    }
+
+    peekOrUndef(depth = 0): T | undefined {
+        return this.data[this.data.length - 1 - depth];
+
+    }
+
+    peekOr(depth = 0, defaultValueOrCreator: T | (() => T)): T {
+        const id = this.data.length - 1 - depth;
+        const value = id < 0 ? undefined : this.data[id];
+        return value === undefined
+            ? (isFunction(defaultValueOrCreator) ? defaultValueOrCreator() : defaultValueOrCreator)
+            : value;
+    }
+
+    *values(): IterableIterator<T> {
+        for (let id = this.data.length - 1; id >= 0; id--)
+            yield this.data[id];
+    }
+
+    *[Symbol.iterator](): IterableIterator<T> {
+        yield* this.values();
+    }
+
+    toArray(): T[] {
+        return [...this.data];
+    }
+
+    isEmpty(): boolean {
+        return this.length === 0;
     }
 
     clear() {
-        this.list.length = 0;
+        this.data.length = 0;
     }
 }
