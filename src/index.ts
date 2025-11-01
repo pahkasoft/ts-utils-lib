79a17390f99d7317bbd8af264eadddf8c4b8c8ee
PahkaSoft <77819265+pahkasoft@users.noreply.github.com>
2025-11-01 21:02:22 +0200

    feat: add getLibInfo().

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ export * as Device from "./web/device";
 export * as Guard from "./guard"
 export * as Utils from "./utils";
 export * from "./core";
+
+declare const __LIB_INFO__: string;
+
+export function getLibInfo(): string {
+    return __LIB_INFO__ ?? "";
+}
